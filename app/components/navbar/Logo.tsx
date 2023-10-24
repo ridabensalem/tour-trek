d548bfe1f9c021fd3322f6e1a7a0e8ce7b9b4bb5
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-10-24 13:17:40 +0100

    Make the logo responsive

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
   const router = useRouter();
 
   return ( 
+      <div className="Logo_container md:text-2xl ">
+      <a className=" font-bold" onClick={() => router.push('/')}> <span className="text-blue-600 ">
+      <span className="text-rose-500 ">Tour </span> Trek</span> 
       
-      <h1 className="text-2xl font-bold" onClick={() => router.push('/')}> 
-      <span className="text-rose-500 ">Tour </span> 
-      <span className="text-blue-600 ">Trek</span> 
-      </h1>
+      </a>
+      </div>
    );
 }
  
