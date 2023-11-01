0f7c75a22aac0528b0dbdf36ba0a40c03d13c068
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-11-01 13:09:20 +0100

    Add linking between login and register

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -3,7 +3,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
 
-import Button from "../Button";
+import Button from "../elementsUi/Button";
 
 interface ModalProps {
   isOpen?: boolean;
