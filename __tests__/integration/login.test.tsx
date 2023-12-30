4dde13e3dcd2cace39d8f66cb007fc5b16fd5170
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-12-30 21:06:47 +0100

    Add integration tests

---
+++ __tests__/integration/login.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { signIn } from 'next-auth/react';
+import LoginModal from '../../app/components/modals/LoginModal';
+
+jest.mock('next-auth/react', () => ({
+  signIn: jest.fn(),
+}));
+
+
+
+describe('LoginModal', () => {
+  it('allows login when email and password are valid', async () => {
+    const { getByLabelText, getByText } = render(<LoginModal />);
+
+    fireEvent.change(getByLabelText('Email'), { target: { value: 'test@example.com' } });
+    fireEvent.change(getByLabelText('Password'), { target: { value: 'password123' } });
+    fireEvent.click(getByText('Login'));
+    expect(signIn).toHaveBeenCalled();
+  });
+
+  it('prevents login when email is invalid', async () => {
+    const { getByLabelText, getByText } = render(<LoginModal />);
+
+    fireEvent.change(getByLabelText('Email'), { target: { value: 'invalid email' } });
+    fireEvent.change(getByLabelText('Password'), { target: { value: 'password123' } });
+    fireEvent.click(getByText('Login'));
+    expect(signIn).not.toHaveBeenCalled();
+
+  });
+
+  it('prevents login when password is invalid', async () => {
+    const { getByLabelText, getByText } = render(<LoginModal />);
+
+    fireEvent.change(getByLabelText('Email'), { target: { value: 'test@example.com' } });
+    fireEvent.change(getByLabelText('Password'), { target: { value: '' } });
+    fireEvent.click(getByText('Login'));
+
+    expect(signIn).not.toHaveBeenCalled();
+  });
+});
+
