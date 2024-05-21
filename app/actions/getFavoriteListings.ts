f33fd45740e2f09dffc5e186d1dd11b489cabbaf
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-05-21 16:49:45 +0100

    clean and add comments

--- app/actions/getFavoriteListings.ts
+++ app/actions/getFavoriteListings.ts
@@ -1,17 +1,22 @@
 import prisma from "@/app/libs/prismadb";
-
 import getCurrentUser from "./getCurrentUser";
 
-//get the current session 
+// Import the 'prisma' instance from the 'prismadb' module
 
+// Import the 'getCurrentUser' function from the './getCurrentUser' module
+
+// Function to get favorite listings
 export default async function getFavoriteListings() {
   try {
+    // Get the current user
     const currentUser = await getCurrentUser();
 
+    // If there is no current user, return an empty array
     if (!currentUser) {
       return [];
     }
 
+    // Find the favorite listings based on the current user's favoriteIds
     const favorites = await prisma.listing.findMany({
       where: {
         id: {
@@ -20,13 +25,16 @@ export default async function getFavoriteListings() {
       }
     });
 
+    // Convert the 'createdAt' property of each favorite listing to a string
     const safeFavorites = favorites.map((favorite) => ({
       ...favorite,
       createdAt: favorite.createdAt.toString(),
     }));
 
+    // Return the safe favorites
     return safeFavorites;
   } catch (error: any) {
+    // Throw an error if any exception occurs
     throw new Error(error);
   }
 }
