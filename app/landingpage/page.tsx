6bf09cfd3be3de954e25279a810eb6ec47fc3c2c
fatima zzahra <fatimazzahraabi@gmail.com>
2024-05-22 17:58:39 +0200

    landing page layout

---
+++ app/landingpage/page.tsx
@@ -0,0 +1,13 @@
+
+import ClientOnly from "@/app/components/elementsUi/ClientOnly";
+
+const LandingPage = async () => {
+
+  return (
+    <ClientOnly>
+      <h1> Hello </h1>
+    </ClientOnly>
+  );
+}
+ 
+export default LandingPage;
