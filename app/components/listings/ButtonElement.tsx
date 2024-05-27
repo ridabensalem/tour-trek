dd05cb230e1f57fef37c06f3e81d36efdacbb577
fatima zzahra <fatimazzahraabi@gmail.com>
2024-05-27 09:44:28 +0200

    add a new sections

---
+++ app/components/listings/ButtonElement.tsx
@@ -0,0 +1,10 @@
+const ButtonElement = () => {
+    return (
+        <div className="mt-4 mb-6 flex justify-end">
+            <button className="bg-blue-700 text-white px-4 py-2 rounded-lg flex items-center">
+                Discover more <span className="ml-2">→</span>
+            </button>
+        </div> 
+    );
+    };
+    export default ButtonElement;
--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -1,13 +1,24 @@
-
 import ClientOnly from "@/app/components/elementsUi/ClientOnly";
+import ListingTrend from "../components/listings/ListingTrend";
+import ListingOffer from "../components/listings/ListingOffer";
+import ListingInspiration from "../components/listings/ListingInspiration";
+import TitleElement from "../components/listings/TitleElement";
+
+
+
 
 const LandingPage = async () => {
 
   return (
     <ClientOnly>
-      <h1> Hello </h1>
+      <TitleElement text="Trending destinations" />
+      <ListingTrend/>
+      <TitleElement text="Discover the offers" />
+      <ListingOffer/>
+      <TitleElement text="Get inspiration for your next trip" />
+      <ListingInspiration/>
     </ClientOnly>
   );
 }
- 
+
 export default LandingPage;
