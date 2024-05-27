77fdd4f8c97aae62eee4ca48b694cf02659087c7
fatima zzahra <fatimazzahraabi@gmail.com>
2024-05-27 10:47:04 +0200

    add changes

--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import ClientOnly from "@/app/components/elementsUi/ClientOnly";
 import ListingTrend from "../components/listings/ListingTrend";
 import ListingOffer from "../components/listings/ListingOffer";
@@ -22,3 +23,26 @@ const LandingPage = async () => {
 }
 
 export default LandingPage;
+=======
+import NavBarLandingpage from '@/app/components/navbar/NavBarLandingpage';
+import Headline from '../components/navbar/Headline';
+import Footer from '../components/navbar/Footer';
+import SocialProof from '../components/navbar/SocialProof';
+
+
+const Page = async () => {
+    return (
+      <>
+        <NavBarLandingpage/>
+        <Headline/>
+        <SocialProof/>
+
+
+
+        <Footer />
+      </>
+    );
+  }
+  
+  export default Page;
+>>>>>>> Stashed changes
