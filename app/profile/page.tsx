1107b83f0123299d6e1bcdea5a154d7efecca297
MustaphaJAM <mustapha.mj.825@gmail.com>
2024-06-07 10:40:57 +0100

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,24 +1,21 @@
 "use client";
-import React, { useState } from "react";
-import Sidebar from "../components/profileComponents/Sidebar";
-import Security from "../components/profile/security";
-import ProfileFrameElement from "../components/profile/profileFrame";
-import Payments from "../components/profile/Payment";
-import Preferences from "../components/profile/Preferences";
-import getUserProfile from "../actions/getUserProfile";
-// import PersonalInfo from "../components/profile/PersonalInfo"
+import React, { useState } from 'react';
+import Sidebar from '../components/profileComponents/Sidebar';
+import Security from '../components/profile/security';
+import Payments from '../components/profile/Payment';
+import Preferences from '../components/profile/Preferences';
+import getUserProfile from '../actions/getUserProfile';
 
 const ProfilePage = () => {
-  const [currentScreen, setCurretScreen] = useState("PersonalInfo");
+  const [currentScreen, setCurrentScreen] = useState('PersonalInfo');
 
   return (
     <div className="container mx-auto max-w-screen-lg flex">
-      <Sidebar setCurretScreen={setCurretScreen} />
+      <Sidebar setCurretScreen={setCurrentScreen} />
       <main className="flex-grow">
-        {currentScreen === "Security" && <Security />}
-        {currentScreen === "Payments" && <Payments />}
-        {currentScreen === "Preferences" && <Preferences />}
-        {/* {currentScreen === "PersonalInfo" && <PersonalInfo user={getUserProfile}/>} */}
+        {currentScreen === 'Security' && <Security />}
+        {currentScreen === 'Payments' && <Payments />}
+        {currentScreen === 'Preferences' && <Preferences />}
       </main>
     </div>
   );
