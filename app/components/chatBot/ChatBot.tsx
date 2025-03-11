d010b5f479cfe7b084f6c004c31e486cbf1f7ea2
ouassima <elyakoubi.ouassima@gmail.com>
2025-03-11 17:43:11 +0000

    update .example.env file + deleting unnecessary comments

--- app/components/chatBot/ChatBot.tsx
+++ app/components/chatBot/ChatBot.tsx
@@ -11,8 +11,6 @@ import {
   TypingIndicator,//This shows a typing indicator when ChatGPT is processing the request.
 } from '@chatscope/chat-ui-kit-react';
 
-
-
 // Define the system message type
 interface SystemMessage {
   role: string;
@@ -44,7 +42,6 @@ const ChatBot =()=> {
       position: 'single',
     },
   ]);
-
   const [isTyping, setIsTyping] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [showPopup, setShowPopup] = useState<boolean>(true)
@@ -54,8 +51,6 @@ const ChatBot =()=> {
     "Is Brazil a safe country?",
   ];
   const [showPredefinedQuestions, setShowPredefinedQuestions] = useState<boolean>(true);
-
-
   const toggleChatbot = () => {
     setIsOpen(!isOpen);
     setShowPopup(false); //Hide pop-up when chatbot is opened
@@ -106,10 +101,9 @@ const ChatBot =()=> {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const data = await response.json();
-      console.log(data);
+    const data = await response.json();
 
-      setMessages([
+    setMessages([
         ...chatMessages,
         {
           message: data.choices[0].message.content,
@@ -124,17 +118,6 @@ const ChatBot =()=> {
       setIsTyping(false);
     }
   }
-
-  /* useEffect(() => {
-    // Hide the pop-up when the chatbot is opened
-    if (isOpen) {
-      setShowPopup(false);
-    } else {
-      // If not opened, show pop-up for 5 seconds
-      const timer = setTimeout(() => setShowPopup(false), 20000);
-      return () => clearTimeout(timer);
-    }
-  }, [isOpen]);  */
   const handlePredefinedQuestionClick = (question: string) => {
     handleSend(question); // Send the selected predefined question as a message
     setShowPredefinedQuestions(false);// Hides questions after one is selected.
@@ -142,19 +125,19 @@ const ChatBot =()=> {
 
   return (
     <div className="fixed bottom-5 right-5 z-50">
-   {/* Conditional pop-up message */}
-   {showPopup && (
-  <div className="absolute bottom-16 right-0 animate-bounce ">
-    <div className="bg-blue-500 text-white font-semibold px-4 py-2 rounded-lg shadow-lg
+      {/* Conditional pop-up message */}
+      {showPopup && (
+      <div className="absolute bottom-16 right-0 animate-bounce ">
+        <div className="bg-blue-500 text-white font-semibold px-4 py-2 rounded-lg shadow-lg
                     text-sm min-w-[200px] max-w-[250px] text-center whitespace-nowrap relative">
-      Hello 👋, Need help? Ask me!
+            Hello 👋, Need help? Ask me!
       {/* Speech Bubble Tail */}
-      <div className="absolute bottom-[-8px] right-3 transform -translate-x-1/2  
+        <div className="absolute bottom-[-8px] right-3 transform -translate-x-1/2  
                     w-0 h-0 border-l-8 border-l-transparent
                     border-r-8 border-r-transparent border-t-8 border-t-blue-500"></div>
-    </div>
-  </div>
-)}
+        </div>
+      </div>
+      )}
       {/* Floating Chatbot Icon */}
       <div
         className="bg-blue-500 text-white w-12 h-12 rounded-full flex items-center justify-center shadow-lg cursor-pointer hover:bg-rose-500"
@@ -165,13 +148,13 @@ const ChatBot =()=> {
 
       {/* Chat Window */}
       {isOpen && (
-        <div className="fixed bottom-20 z-50 right-5 w-[350px] bg-white shadow-lg rounded-lg">
-          <div className="h-[400px] overflow-y-auto shadow-lg rounded-lg ">
-            <MainContainer style={{
+      <div className="fixed bottom-20 z-50 right-5 w-[350px] bg-white shadow-lg rounded-lg">
+        <div className="h-[400px] overflow-y-auto shadow-lg rounded-lg ">
+          <MainContainer style={{
                     border:'none',
                   }
-                  }>
-              <ChatContainer>
+          }>
+            <ChatContainer>
               <ConversationHeader  style={{
                     backgroundColor:'#3B82F6',
                     boxShadow: `var(--shadow-lg)`,
@@ -184,16 +167,16 @@ const ChatBot =()=> {
                   }
                   } 
                 />
-                </ConversationHeader>
-                <MessageList
+              </ConversationHeader>
+              <MessageList
                   style={{
                   marginTop:"6px",
                   }}
                   scrollBehavior="smooth"
                   typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
-                >
+              >
                   {messages.map((message, i) => (
-                    <Message
+                <Message
                       key={i}
                       model={{
                         message: message.message,
@@ -202,37 +185,37 @@ const ChatBot =()=> {
                         position: message.position,
                       }}
                       
-                    />
-                  ))}
-                  {showPredefinedQuestions && (
-                  <div className='fixed bottom-4 absolute'>
-                    <p className="text-xs text-gray-600 pb-2">Click a question to ask:</p>
-                    <div className="grid grid-cols-3 gap-1 pr-3">
-                    {predefinedQuestions.map((question, index) => (
-                      <button
+                />
+                ))}
+                {showPredefinedQuestions && (
+                <div className='fixed bottom-4 absolute'>
+                  <p className="text-xs text-gray-600 pb-2">Click a question to ask:</p>
+                  <div className="grid grid-cols-3 gap-1 pr-3">
+                  {predefinedQuestions.map((question, index) => (
+                    <button
                         key={`predefined-${index}`}
                         onClick={() => handlePredefinedQuestionClick(question)}
                         className="bg-blue-200 text-blue-600 px-2 py-1 rounded-full text-xs hover:bg-gray-400 focus:outline-none border border-gray-400"
-                      >
+                    >
                         {question}
-                      </button>
+                    </button>
                     ))}
-                    </div>
                   </div>
+                </div>
                 )}
-                </MessageList>               
-                <MessageInput
+              </MessageList>               
+              <MessageInput
                   placeholder="Type message here"
                   onSend={handleSend}
                   attachButton={false}
                   style={{
                     padding:'6px',
                   }}
-                />
-              </ChatContainer>
-            </MainContainer>
-          </div>
+              />
+            </ChatContainer>
+          </MainContainer>
         </div>
+      </div>
       )}
     </div>
   );
